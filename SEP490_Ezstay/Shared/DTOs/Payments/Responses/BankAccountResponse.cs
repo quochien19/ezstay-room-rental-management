ce7180f8a85f892668a0cfcdffc24dcbe560f542
Quoc Hien <hiennqce172010@fpt.edu.vn>
2025-11-27 17:54:27 +0700

    ok

--- SEP490_Ezstay/Shared/DTOs/Payments/Responses/BankAccountResponse.cs
+++ SEP490_Ezstay/Shared/DTOs/Payments/Responses/BankAccountResponse.cs
@@ -5,11 +5,12 @@ public class BankAccountResponse
     public Guid Id { get; set; } 
     public Guid UserId { get; set; }
     public BankGatewayResponse BankGateway { get; set; }
-    
     public string AccountNumber { get; set; }
     public decimal Amount { get; set; }
     public string Description { get; set; }
     public string ImageQR { get; set; }
-    public DateTime CreatedAt { get; set; } 
+    
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
