fd61dd9ebe561184f2b9a5d6dcedf7ea0ae2892e
Hien <hiennqce172010@fpt.edu.vn>
2025-12-11 15:45:23 +0700

    ok

--- SEP490_Ezstay/UtilityBillAPI/Program.cs
+++ SEP490_Ezstay/UtilityBillAPI/Program.cs
@@ -141,7 +141,7 @@ builder.Services.AddCors(options =>
                 "https://ezstay-fe.vercel.app",
                 "https://payment-api-r4zy.onrender.com",
                 https://bill-half.onrender.com,
-             https://bill-half.onrender.com
+    
               )
               .AllowAnyHeader()
               .AllowAnyMethod()
